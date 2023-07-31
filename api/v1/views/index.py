df20e4781fb59bad37a23b0c28db416f08239913
201496441 <magorothabo1@gmail.com>
2023-07-31 21:33:10 +0200

    update
    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
-"""The index folder package"""
-
-from flask import jsonify
+"""index.py to connect to API"""
 from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
 from models import storage
-from models.amenity import Amenity
-from models.place import Place
-from models.user import User
-from models.state import State
-from models.review import Review
-from models.city import City
 
 
-@app_views.route('/status')
-def status():
-    """Return the status of the page"""
-    status = {
-            'status': 'OK'
-            }
-    return jsonify(status)
+hbnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
-@app_views.route('/stats')
-def list_counnt():
-    """Return the number of each objects in database"""
-    objects = {
-            'amenities': Amenity,
-            'cities': City,
-            'places': Place,
-            'reviews': Review,
-            'states': State,
-            'users': User
-            }
-    my_dict = {}
-    for key, value in objects.items():
-        my_dict[key] = storage.count(value)
-    return jsonify(my_dict)
+@app_views.route('/status', strict_slashes=False)
+def hbnbStatus():
+    """hbnbStatus"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    """hbnbStats"""
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
+
+if __name__ == "__main__":
+    pass
